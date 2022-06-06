fe216083050736e7f8a135f27d2e2560e6af991b
Fawaz Oduola <oduolafawaz@gmail.com>
2022-06-06 16:23:03 +0000

    task 9 done

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    max_value = my_list[0]
+    for i in my_list:
+        if i > max_value:
+            max_value = i
+    return max_value
