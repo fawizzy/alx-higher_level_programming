852f5ba2b56fc8840234799ace64d1da5d867f17
Fawaz Oduola <oduolafawaz@gmail.com>
2022-06-22 18:40:49 +0000

    tasks done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class square"""
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a square
+
+        Args:
+            size (int): The size of a new square
+        """
+
+        self.__size = size
